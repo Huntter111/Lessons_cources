dcd498c4ec435b0ce414af0af866994d460384ff
Volodymyr-Hunter <63543837+Huntter111@users.noreply.github.com>
2022-11-16 21:56:31 +0200

    fixed: bugs

--- jaguar/js/form.js
+++ jaguar/js/form.js
@@ -1,3 +1,30 @@
+// $(document).ready(function () {
+// 	$('.g-form').on('submit', function (event) {
+// 		event.preventDefault();
+// 		let captcha = grecaptcha.getResponse();
+
+// 		if (!captcha.length) {
+// 			$('#recaptchaError').text('* Ви не пройшли перевірку капчі');
+// 		} else {
+// 			$('#recaptchaError').text('');
+
+// 			let dataForm = $(this).serialize();
+
+// 			$.ajax({
+// 				url: '/form.php',
+// 				method: 'post',
+// 				dataType: 'html',
+// 				data: dataForm,
+// 				success: function (data) {
+// 					console.log(data);
+
+// 					grecaptcha.reset();
+// 				},
+// 			});
+// 		}
+// 	});
+// });
+
 $(
 	(function () {
 		$('.g-form').submit(function (event) {
@@ -32,93 +59,6 @@ $(
 
 			// FormData
 			let fd = new FormData(form);
-
-			$.ajax({
-				url: appLink,
-				type: 'POST',
-				data: fd,
-				processData: false,
-				contentType: false,
-				beforeSend: function () {
-					if (fd.get('honeypot').length) {
-						return false;
-					} else {
-						fd.delete('honeypot');
-					}
-
-					// Показываем прелоадер
-					preloader.css('opacity', '1');
-
-					// Делаем неактивной кнопку отправки
-					submitButton.prop('disabled', true);
-
-					// валидация других полей.
-				},
-			})
-				.done(function (res, textStatus, jqXHR) {
-					if (jqXHR.readyState === 4 && jqXHR.status === 200) {
-						// Прячем заголовок формы
-						formTitle.css({
-							display: 'none',
-						});
-
-						// Прячем прелоадер
-						preloader.css('opacity', '0');
-
-						// Выводим ответ формы.
-						formRespond.html(successRespond).css('color', '#37b599'),
-							('font-size', '30px');
-
-						// Возвращаем активность кнопке отправки
-						submitButton.prop('disabled', false);
-
-						// Очищаем поля формы
-						form.reset();
-					} else {
-						formTitle.css({
-							display: 'none',
-						});
-						formRespond.html(errorRespond).css('color', '#c64b4b');
-						preloader.css('opacity', '0');
-						setTimeout(() => {
-							formRespond.css({
-								display: 'none',
-							});
-							formTitle.css({
-								display: 'block',
-							});
-
-							submitButton.prop('disabled', false);
-						}, 2000);
-
-						console.log('Гугл не ответил статусом 200');
-					}
-				})
-				.fail(function (res, textStatus, jqXHR) {
-					formTitle.css({
-						display: 'none',
-					});
-					preloader.css('opacity', '0');
-					formRespond
-						.html(
-							"Повідомлення НЕ відправлено. Будь-ласка напишіть нам на <a class='form__link' href='mailto:nabir@robotajaguarlandrover.com.ua'>Email</a>",
-						)
-						.css('color', '#c64b4b');
-					setTimeout(() => {
-						formRespond.css({
-							display: 'none',
-						});
-						formTitle.css({
-							display: 'block',
-						});
-						submitButton.prop('disabled', false);
-					}, 5000);
-
-					console.log(
-						'Не вдалось виконати запит за вказаним шляхом скрипту',
-					);
-				});
-			event.preventDefault();
 			let captcha = grecaptcha.getResponse();
 
 			if (!captcha.length) {
@@ -139,6 +79,93 @@ $(
 						grecaptcha.reset();
 					},
 				});
+			}
+			if (!captcha.length) {
+				$.ajax({
+					url: appLink,
+					type: 'POST',
+					data: fd,
+					processData: false,
+					contentType: false,
+					beforeSend: function () {
+						if (fd.get('honeypot').length) {
+							return false;
+						} else {
+							fd.delete('honeypot');
+						}
+
+						// Показываем прелоадер
+						preloader.css('opacity', '1');
+
+						// Делаем неактивной кнопку отправки
+						submitButton.prop('disabled', true);
+
+						// валидация других полей.
+					},
+				})
+					.done(function (res, textStatus, jqXHR) {
+						if (jqXHR.readyState === 4 && jqXHR.status === 200) {
+							// Прячем заголовок формы
+							formTitle.css({
+								display: 'none',
+							});
+
+							// Прячем прелоадер
+							preloader.css('opacity', '0');
+
+							// Выводим ответ формы.
+							formRespond.html(successRespond).css('color', '#37b599'),
+								('font-size', '30px');
+
+							// Возвращаем активность кнопке отправки
+							submitButton.prop('disabled', false);
+
+							// Очищаем поля формы
+							form.reset();
+						} else {
+							formTitle.css({
+								display: 'none',
+							});
+							formRespond.html(errorRespond).css('color', '#c64b4b');
+							preloader.css('opacity', '0');
+							setTimeout(() => {
+								formRespond.css({
+									display: 'none',
+								});
+								formTitle.css({
+									display: 'block',
+								});
+
+								submitButton.prop('disabled', false);
+							}, 100);
+
+							console.log('Гугл не ответил статусом 200');
+						}
+					})
+					.fail(function (res, textStatus, jqXHR) {
+						formTitle.css({
+							display: 'none',
+						});
+						preloader.css('opacity', '0');
+						formRespond
+							.html(
+								"Повідомлення НЕ відправлено. Будь-ласка напишіть нам на <a class='form__link' href='mailto:nabir@robotajaguarlandrover.com.ua'>Email</a>",
+							)
+							.css('color', '#c64b4b');
+						setTimeout(() => {
+							formRespond.css({
+								display: 'none',
+							});
+							formTitle.css({
+								display: 'block',
+							});
+							submitButton.prop('disabled', false);
+						}, 5000);
+
+						console.log(
+							'Не вдалось виконати запит за вказаним шляхом скрипту',
+						);
+					});
 			}
 		});
 	})(jQuery),
@@ -167,30 +194,3 @@ function processInput(phone) {
 		if (!iti.isValidNumber()) phone.setCustomValidity('Невірний номер');
 	});
 }
-
-// $(document).ready(function () {
-// 	$('#g-form-1').on('submit', function (event) {
-// 		event.preventDefault();
-// 		let captcha = grecaptcha.getResponse();
-
-// 		if (!captcha.length) {
-// 			$('#recaptchaError').text('* Ви не пройшли перевірку капчі');
-// 		} else {
-// 			$('#recaptchaError').text('');
-
-// 			let dataForm = $(this).serialize();
-
-// 			$.ajax({
-// 				url: '/form.php',
-// 				method: 'post',
-// 				dataType: 'html',
-// 				data: dataForm,
-// 				success: function (data) {
-// 					console.log(data);
-
-// 					grecaptcha.reset();
-// 				},
-// 			});
-// 		}
-// 	});
-// });
