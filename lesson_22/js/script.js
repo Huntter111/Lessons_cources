b8757fc516fba2f662afb0d2be42f87b143e6508
Volodymyr-Hunter <63543837+Huntter111@users.noreply.github.com>
2022-11-30 12:51:21 +0200

    create: lesson_22

---
+++ lesson_22/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Завдання №1
+let colorEyesPerson;
+
+// Завдання №2
+let user;
+let userName;
+
+user = 'Вася';
+userName = user;
+
+console.log(userName);
+
+if (true) {
+	var size = 15;
+}
+console.log(size);
