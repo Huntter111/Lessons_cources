027f0c3877f3c4a7e9783e9a9b3d1b56211a9aab
Volodymyr-Hunter <63543837+Huntter111@users.noreply.github.com>
2022-12-03 20:31:55 +0200

    create: lesson_23

---
+++ lesson_23/js/script.js
@@ -0,0 +1,102 @@
+'use strict';
+/*
+// Числа від 1 до 5
+
+// Метод №1
+let a = 1;
+while (a <= 5) {
+	console.log(a);
+	a++;
+}
+console.log('________________');
+
+// Метод №2
+let b = 1;
+while (b <= 5) console.log(b++);
+console.log('________________');
+
+// Метод №3
+let c = 1;
+do {
+	console.log(c);
+	c++;
+} while (c < 5);
+console.log('________________');
+
+// Метод №4
+for (let d = 1; d <= 5; d++) {
+	console.log(d);
+}
+console.log('________________');
+
+// Метод 5
+let abss = 1;
+while (abss) {
+	console.log(abss);
+	abss++;
+	if (abss == 6) break;
+}
+*/
+// let num = 0;
+
+// while (num < 3) {
+// 	console.log(`Число: ${num}`);
+// 	num++;
+// 	if (num == 5) break; // Для підстраховки )
+// }
+
+// for (let num1 = 0; num1 < 3; num1++) {
+// 	console.log(`Число: ${num1}`);
+// }
+
+// breakOne: for (let num = 0; num < 2; num++) {
+// 	for (let size = 0; size < 3; size++) {
+// 		if (size == 1) {
+// 			break breakOne;
+// 		}
+// 		console.log(size);
+// 	}
+// }
+/*
+// Задача 1. Отримати правильне значення округлення
+let numOne = 1.005 + Number.EPSILON;
+numTwo = Math.round(numOne * 100) / 100;
+console.log(numTwo);
+
+// Задача 2. Отримати 135.58 з рядка
+let valueHave = '135.58px';
+let valueResult = parseFloat(valueHave);
+console.log(valueResult);
+
+// Задача 3. Створити правельне вітвлення
+let value = 58 + 'Фрілансер';
+if (isNaN(value)) {
+	console.log('Результат NaN');
+}
+// Задача 4. Знайти максимальне число з 10, 58, 39, -150, 0)
+let numberMax = Math.max(10, 58, 39, -150, 0);
+console.log(numberMax);
+
+// Задача 5. Округлить число 58.858 до числа 58
+let number = 58.858;
+let numberResult = Math.floor(number);
+console.log(numberResult);
+*/
+// Задача 2. Отримати символ "н" з рядка
+// let text = 'фрилансер';
+// console.log(text.slice(5, -3));
+// Задача 3. Ждемо 579
+// let text = 123 + '456';
+// console.log(text);
+
+// Задача 4. Отримати рядок у верхньому регістрі
+// let text = 'фрилансер';
+// console.log(text.toUpperCase());
+
+// Задача 5. Отримати підрядок "лан" з
+// let text = 'фрилансер';
+// console.log(text.slice(3, 6));
+
+// Задача 6. true or false?
+// let text = 'фрилансер';
+// console.log(text.includes('лан', 4));
